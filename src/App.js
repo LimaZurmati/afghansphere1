34adad8f4d4ba3f6b04c3679c2b3d024f15814f1
Lima Zurmati <urzalalima@gmail.com>
2024-10-25 19:01:24 +0000

    Create switch inside container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
+        <Switch>
         <h1>Home page</h1>
         <h1>Sign In</h1>
+        </Switch>
       </Container>
     </div>
   );
