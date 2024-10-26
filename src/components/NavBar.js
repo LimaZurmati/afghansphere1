e5989b15f03f7805cf450a65e3623c83828fd407
Lima Zurmati <urzalalima@gmail.com>
2024-10-26 17:03:44 +0000

    Install axios

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,44 @@
-
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import styles from "../styles/NavBar.module.css"; 
+import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-      <NavLink to="/">
+        <NavLink to="/">
         <Navbar.Brand className={styles.NavBrand}
            alt="logo">
           
            <span>Afghanspher</span>
            <i class="fa-brands fa-skyatlas"></i>
         </Navbar.Brand>
+
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink              exact
+            <NavLink
+              exact
               className={styles.NavLink}
               activeClassName={styles.Active}
-               to ="/">
+              to="/"
+            >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink className={styles.NavLink}
-              activeClassName={styles.Active} to ="signin">
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
               <i className="fas fa-sign-in-alt"></i>Sign in
             </NavLink>
-            <NavLink to ="signup" className={styles.NavLink}
-              activeClassName={styles.Active}>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
               <i className="fas fa-user-plus"></i>Sign up
             </NavLink>
           </Nav>
