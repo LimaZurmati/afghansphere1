0472967e5b64ca298b6059c370a67ec704e42da1
Lima Zurmati <urzalalima@gmail.com>
2024-10-28 20:02:29 +0000

    Include logic to retrieve post data.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,10 +7,27 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 
-function PostPage() {
+
   // Add your logic here
+function PostPage() {
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
 
-
+useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: post }] = await Promise.all([
+              axiosReq.get(`/posts/${id}`),
+            ]);
+            setPost({ results: [post] });
+            console.log(post);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [id]);    
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
